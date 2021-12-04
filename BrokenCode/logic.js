6f052b617c1a7053ce03ff2d931531fee3287fb7
wreising <wreising@mac.com>
2021-12-04 08:53:31 -0800

    done

--- BrokenCode/logic.js
+++ BrokenCode/logic.js
@@ -3,25 +3,26 @@ $("#factButton").on("click", function() {
 	$("#factText").text(booFacts[number])
 })
 
-var booFacts = ["Boo is a pomeranian, Boo's best friend is another pomeranian named Buddy, Boo the Pomeranian was born on March 16, making him a Pisces, Boo's favourite food is grass, Boo has released two books" ]
+var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16, making him a Pisces", "Boo's favourite food is grass", "Boo has released two books" ]
 
 $("#textPink").on("click", function() {
-	$("#funText").css("color", pink)
-})
+	$("#funText").css("color", "pink")
+});
 
 $("#textOrange").on("click", function() {
-$("#funText").css("color", "orange")
-})
+	$("#funText").css("color", "orange")
+});
+
 $("#textGreen").on("click", function() {
 	$("#funText").css("color", "green")
 })
 
 
-$("#boxGrow").on(click, function() {
+$("#boxGrow").on("click", function() {
 	$("#box").animate({height:"+=35px", 
 		width:"+=35px"}, "fast");
 })
 
-$("#boxShrink").on(click, function() {
+$("#boxShrink").on("click", function() {
 	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
 })
